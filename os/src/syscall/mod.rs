173f1fec14b83ab44acae4b88e8cb9519b617831
Leo Cheng <chengkelfan@qq.com>
2025-05-03 13:40:06 +0800

    update: cp from 2024a

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -152,3 +152,7 @@ pub fn syscall(syscall_id: usize, args: [usize; 4]) -> isize {
         _ => panic!("Unsupported syscall_id: {}", syscall_id),
     }
 }
+
+mod util;
+#[allow(unused_imports)]
+use util::*;
